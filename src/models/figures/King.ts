d1d67815ff786682c3b9e09094983bd3329054fe
Konstantin Krivilev <inferno750@mail.ru>
2024-03-16 22:46:35 +0300

    add King's movement

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -15,6 +15,17 @@ export class King extends Figure {
     if (!super.canMove(target)) {
       return false;
     }
-    return true;
+    const dx = Math.abs(target.x - this.cell.x);
+    const dy = Math.abs(target.y - this.cell.y);
+
+    if (
+      (dx === 1 && dy === 0) ||
+      (dx === 0 && dy === 1) ||
+      (dx === 1 && dy === 1)
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
